a0e9e9622292ef7d68f30f79dd6561184621119a
Arseniy Molodetskiy <molodeckij.b.2017@gmail.com>
2022-04-16 03:03:32 +0300

    added handling for exceptions

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -2,7 +2,12 @@ package io.pleo.antaeus.core.services
 
 import dev.inmo.krontab.KronScheduler
 import dev.inmo.krontab.doWhile
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 
 class BillingService(
     private val paymentProvider: PaymentProvider,
@@ -10,14 +15,47 @@ class BillingService(
     private val customerService: CustomerService
 ) {
 
-    suspend fun init(scheduler: KronScheduler){
+    private val failedInvoices: ArrayList<Invoice> = ArrayList()
+
+    suspend fun init(scheduler: KronScheduler) {
         scheduler.doWhile {
             chargePayments()
             true // true - repeat on next time
         }
     }
-    fun chargePayments() {
+
+    private fun chargePayments() {
         val invoices = invoiceService.fetchAll();
-        invoices.forEach { paymentProvider.charge(it) }
+        invoices.forEach { processInvoice(it) }
+    }
+
+    private fun processInvoice(invoice: Invoice) {
+        try {
+            val result = paymentProvider.charge(invoice)
+            if (result) {
+                val paidInvoice = Invoice(
+                    id = invoice.id,
+                    customerId = invoice.customerId,
+                    amount = invoice.amount,
+                    status = InvoiceStatus.PAID
+                )
+                invoiceService.updateInvoice(paidInvoice)
+            } else {
+                val paidInvoice = Invoice(
+                    id = invoice.id,
+                    customerId = invoice.customerId,
+                    amount = invoice.amount,
+                    status = InvoiceStatus.PENDING
+                )
+                invoiceService.updateInvoice(paidInvoice)
+                failedInvoices.add(invoice)
+            }
+        } catch (customerNotFoundException: CustomerNotFoundException) {
+            return
+        } catch (currencyMismatchException: CurrencyMismatchException) {
+            return
+        } catch (networkException: NetworkException) {
+            failedInvoices.add(invoice)
+        }
     }
 }
